5b30fb7bb17ca3e8a9f4f2c1017470c04f0c7d7a
Prashant Kumbhar <prashant.kumbhar@prolifics.com>
2022-07-27 16:22:53 +0530

    Initial commit

---
+++ 9_WellFormedTypes/NestingNamespaceswithinOneAnother/NestingNamespaceswithinOneAnother/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestingNamespaceswithinOneAnother
+{
+    //Define the NameSpace AddisonWesley
+    namespace AddisonWesley
+    {
+        //Define the NameSpace AddisonWesley.Michaelis
+        namespace Michaelis
+        {                   
+            namespace EssencialCsharp       // Define the namespace AddisonWesley.Michaelis.EssentialCSharp
+            {
+                class Program               // Declare the class AddisonWesley.Michaelis.EssentialCSharp.Program
+                {
+                    static void Main()      
+                    {
+                        //....
+                    }
+                }
+            }
+        }
+    }
+
+    //Nesting Namespaces Using a Period to Separate Each Identifie
+
+    // Define the namespace AddisonWesley.Michaelis.EssentialCSharp
+    namespace AddisonWesley2.Michaelis2.EssencialCsharp
+    {
+        class Program2
+        { 
+
+        }
+    }
+    // End of AddisonWesley namespace declaration
+}
